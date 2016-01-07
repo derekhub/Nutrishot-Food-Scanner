de5941bc2e407b6b198e15ef44cfd147cf039787
derekhub <derekchoi105@gmail.com>
2016-01-07 02:16:15 -0500

    have working java code to connect to sql database in XAMPP.

--- src/CamFindAPI.java
+++ src/CamFindAPI.java
@@ -33,90 +33,20 @@ public class CamFindAPI {
 
     public String getImageResponse(String token) throws UnirestException {
 
-        // {
-        // "status": "completed",
-        // "name": "pepperoni pizza"
-        // }
-
-        // try {
-        // Thread.sleep(5000); // 1000 milliseconds is one second.
-        // } catch (InterruptedException ex) {
-        // Thread.currentThread().interrupt();
-        // }
-
-        // These code snippets use an open-source library.
-        // http://unirest.io/java
-
-        String tokenUrl = "https://camfind.p.mashape.com/image_responses/" + token;
-
-        System.out.println(tokenUrl);
-
-        HttpResponse<JsonNode> description = Unirest
-                .get("https://camfind.p.mashape.com/image_responses/5k6WX0Yk7rk8zzD3McuT_Q")
-                .header("X-Mashape-Key", "GXAEZZIp78mshXbId2kurGfdvAlvp1XlRjsjsn70l25nGbvS0d")
-                .header("Accept", "application/json").asJson();
-
-        String status = (String) description.getBody().getObject().get("status");
-
-        System.out.println(description.getBody() + " isssss: " + status);
-
-        System.out.println("Status isssss: " + status);
-
-        if (status.equals("completed")) {
-            String name = (String) description.getBody().getObject().get("name");
-            return name;
-
-        } else {
-            String reason = (String) description.getBody().getObject().get("reason");
-            System.out.println("getImageResponse status is 'skipped'. Reason is: " + reason);
-        }
-
-        return null;
-
-    }
-
-    public String temp(String url) throws UnirestException {
-
-        // These code snippets use an open-source library.
-        // http://unirest.io/java
-        HttpResponse<JsonNode> response = Unirest.post("https://camfind.p.mashape.com/image_requests")
-                .header("X-Mashape-Key", "GXAEZZIp78mshXbId2kurGfdvAlvp1XlRjsjsn70l25nGbvS0d")
-                .header("Content-Type", "application/x-www-form-urlencoded").header("Accept", "application/json")
-                .field("focus[x]", "480").field("focus[y]", "640").field("image_request[altitude]", "27.912109375")
-                .field("image_request[language]", "en").field("image_request[latitude]", "35.8714220766008")
-                .field("image_request[locale]", "en_US").field("image_request[longitude]", "14.3583203002251")
-                .field("image_request[remote_image_url]", url).asJson();
-
-        // {
-        // "token": "fhr1thsRYDm6Vgwx2hTgBw",
-        // "url":
-        // "//d1spq65clhrg1f.cloudfront.net/uploads/image_request/image/35/35311/35311614/o.jpg"
-        // }
-
-        // System.out.println(DEBUG: response.getBody());
-
-        String token = (String) response.getBody().getObject().get("token");
-        // System.out.println("TOKEN = " + token);
-
-        // ----------------------------------------------------
-
-        System.out.println("yoloooooooooo " + token);
+        // System.out.println("DEBUG: " + token);
 
         // {
         // "status": "completed",
         // "name": "pepperoni pizza"
         // }
 
-        // try {
-        // Thread.sleep(5000); // 1000 milliseconds is one second.
-        // } catch (InterruptedException ex) {
-        // Thread.currentThread().interrupt();
-        // }
-
         // These code snippets use an open-source library.
         // http://unirest.io/java
-
         String tokenUrl = "https://camfind.p.mashape.com/image_responses/" + token;
+        // String tokenUrl =
+        // "https://camfind.p.mashape.com/image_responses/lAwMibtKlfyjpMIsLP6HSA";
+
+        // System.out.println(tokenUrl);
 
         HttpResponse<JsonNode> description = Unirest.get(tokenUrl)
                 .header("X-Mashape-Key", "GXAEZZIp78mshXbId2kurGfdvAlvp1XlRjsjsn70l25nGbvS0d")
@@ -124,9 +54,9 @@ public class CamFindAPI {
 
         String status = (String) description.getBody().getObject().get("status");
 
-        System.out.println(description.getBody() + " isssss: " + status);
+        System.out.println("DEBUG: returned JSON is: " + description.getBody());
 
-        System.out.println("Status isssss: " + status);
+        // System.out.println("Status isssss: " + status);
 
         if (status.equals("completed")) {
             String name = (String) description.getBody().getObject().get("name");
--- src/MainScan.java
+++ src/MainScan.java
@@ -5,9 +5,15 @@ public class MainScan {
     public static void main(String[] args) throws UnirestException {
 
         CamFindAPI CamFindAPIObj = new CamFindAPI();
-        String token = CamFindAPIObj
-                .getImageRequestToken("http://s3-media1.fl.yelpcdn.com/bphoto/e0b27Zk_u2qpvgPcRy99iw/o.jpg");
-        System.out.println("Debug: token is: " + token);
+        // String token = CamFindAPIObj
+        // .getImageRequestToken("http://s3-media1.fl.yelpcdn.com/bphoto/e0b27Zk_u2qpvgPcRy99iw/o.jpg");
+
+        // System.out.println("Debug: token is: " + token);
+
+        // TODO: hardcoded for now because string concatenation in
+        // getImageResponse is having problems
+        String token = "lAwMibtKlfyjpMIsLP6HSA";
+
         String description = CamFindAPIObj.getImageResponse(token);
         System.out.println(description);
 
